47debba8e34a0cf2a953abf4ce778bf2e991ae44
Garagylja <kein85@gmail.com>
2016-01-08 18:25:05 +0200

    Гонки Создание окна

---
+++ src/main/java/Road.java
@@ -0,0 +1,8 @@
+/**
+ * Created by user on 08.01.2016.
+ */
+import javax.swing.*;
+public class Road extends JPanel {
+	
+
+}
---
+++ src/main/java/Runner.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 02.12.2015.
+ */
+import javax.swing.*;
+
+public class Runner {
+
+	public static void main (String[] args) {
+		JFrame frame = new javax.swing.JFrame ("Java Race");
+		frame.setDefaultCloseOperation (javax.swing.JFrame.EXIT_ON_CLOSE);
+		frame.setSize (1100,600);
+		frame.setVisible (true);
+	}
+}
